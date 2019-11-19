84dea58eb991e125301c40077e3001efb0e10981
sharknia <furychick0@gmail.com>
2019-11-19 17:53:14 +0900

    12.2.4 TypeScript Definition 설치

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,8 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var _ = require("lodash");
 var startup = /** @class */ (function () {
     function startup() {
     }
     startup.main = function () {
-        console.log("HELLO WORLD2");
+        var group = _.groupBy(['one', 'two', 'three'], 'length');
+        console.log(group);
         return 0;
     };
     return startup;
--- HelloWorld.ts
+++ HelloWorld.ts
@@ -1,7 +1,11 @@
+import * as _ from 'lodash';
+
 class startup{
     public static main(): number{
-        console.log("HELLO WORLD2");
+        const group = _.groupBy(['one', 'two', 'three'], 'length');
+        console.log(group);
         return 0;
     }
 }
+
 startup.main();
