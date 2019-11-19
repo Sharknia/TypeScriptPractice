07a9ee5b077262533c0a1682558de2f1a4ff6e4b
sharknia <furychick0@gmail.com>
2019-11-19 17:44:37 +0900

    12.2.3 TaskRunner

---
+++ HelloWorld.js
@@ -0,0 +1,11 @@
+var startup = /** @class */ (function () {
+    function startup() {
+    }
+    startup.main = function () {
+        console.log("HELLO WORLD2");
+        return 0;
+    };
+    return startup;
+}());
+startup.main();
+//# sourceMappingURL=HelloWorld.js.map
---
+++ HelloWorld.ts
@@ -0,0 +1,7 @@
+class startup{
+    public static main(): number{
+        console.log("HELLO WORLD2");
+        return 0;
+    }
+}
+startup.main();
